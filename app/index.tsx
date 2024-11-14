eab944b3693c4d4656932f3b6a6023efcac8e671
Zodan1 <dannyz93@msn.com>
2024-11-14 21:48:56 +0000

    added camera functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,37 @@
 import { useState } from "react";
-import { Button, Image, View, StyleSheet } from "react-native";
+import { Button, Image, View, StyleSheet, Text } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import { StatusBar } from "expo-status-bar";
 
 export default function ImagePickerExample() {
+  // const [permission, requestPermission] = ImagePicker.useCameraPermissions();
+
+  // if (permission?.status !== ImagePicker.PermissionStatus.GRANTED) {
+  //   return (
+  //     <View style={styles.container}>
+  //       <Text>Permission not granted {permission?.status}</Text>
+  //       <StatusBar style="auto" />
+  //       <Button title="Request permission" onPress={requestPermission} />
+  //     </View>
+  //   );
+  // }
+
   const [image, setImage] = useState<string | null>(null);
+
+  const takePhoto = async () => {
+    let cameraResponse = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      mediaTypes: ["images", "videos"],
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(cameraResponse);
+
+    if (!cameraResponse.canceled) {
+      setImage(cameraResponse.assets[0].uri);
+    }
+  };
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -24,6 +52,7 @@ export default function ImagePickerExample() {
   return (
     <View style={styles.container}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
+      <Button title="take picture" onPress={takePhoto} />
       {image && <Image source={{ uri: image }} style={styles.image} />}
     </View>
   );
